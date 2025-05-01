0b2a28e8fcb198db866b88c88d47e04cd9081894
JoshuaSaayo <saayojoshua@gmail.com>
2025-05-01 21:25:05 +0800

    added map selection

--- game/Scripts/game_controller.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	if Global.selected_map:
-		var map = load(Global.selected_map).instantiate()
-		add_child(map)
-	else:
-		# Default map or return to menu
-		get_tree().change_scene_to_file("res://main_menu.tscn")
--- game/Scripts/main_menu.gd
+++ game/Scripts/main_menu.gd
@@ -9,10 +9,6 @@ func _on_start_btn_pressed() -> void:
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
 
-func _ready():
-	$LevelSelection/Level1Btn.connect("pressed", _on_level_selected.bind("res://Maps/map_1.tscn"))
-	$LevelSelection/Level2Btn.connect("pressed", _on_level_selected.bind("res://Scenes/world.tscn"))
-	
-func _on_level_selected(map_path: String):
-	Global.selected_map = map_path  # Store selection
-	get_tree().change_scene_to_file("res://Scenes/game_controller.tscn")  # Or directly to map	
+
+func _on_select_map_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")  # Replace with your actual game scene
