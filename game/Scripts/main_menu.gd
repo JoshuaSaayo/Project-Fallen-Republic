eeab7b40f6d748eedce4b02d4801bcdbf5a8e5fa
JoshuaSaayo <saayojoshua@gmail.com>
2025-05-01 18:26:23 +0800

    added dialogue sheet and map_1

--- game/Scripts/main_menu.gd
+++ game/Scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")  # Replace with your actual game scene
+	get_tree().change_scene_to_file("res://Maps/map_1.tscn")  # Replace with your actual game scene
 
 
 func _on_quit_btn_pressed() -> void:
