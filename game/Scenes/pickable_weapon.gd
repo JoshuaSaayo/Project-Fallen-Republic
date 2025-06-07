1855ec85401dd13e549d2f4521a0f226f198d9c7
StevenAlfon <saayojoshua@gmail.com>
2025-06-07 22:31:44 +0800

    update auto picking up weapon

--- game/Scenes/pickable_weapon.gd
+++ game/Scenes/pickable_weapon.gd
@@ -26,15 +26,12 @@ class_name PickableWeapon
 ## Scale adjustment for this weapon type            
 @export var display_scale: Vector2 = Vector2(0.5, 0.5)
 
-var can_pick_up := false
-
 # Weapon texture naming convention: "{WEAPON_ID}-PICKABLE.png"
 const TEXTURE_PATH = "res://Assets/Guns/pickable_weapons/%s-PICKABLE.png"
 
 func _ready():
 	update_weapon_appearance()
 	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
 
 func update_weapon_appearance():
 	# Load texture if not manually set
@@ -59,17 +56,7 @@ func _get_weapon_texture(id: String) -> Texture2D:
 
 func _on_body_entered(body: Node2D):
 	if body.is_in_group("Player"):
-		can_pick_up = true
-		body.show_pickup_prompt(true, weapon_name)
-
-func _on_body_exited(body: Node2D):
-	if body.is_in_group("Player"):
-		can_pick_up = false
-		body.show_pickup_prompt(false)
-
-func _input(event):
-	if can_pick_up and event.is_action_pressed("interact"):
-		var player = get_tree().get_first_node_in_group("Player")
-		if player and player.has_method("add_weapon_to_inventory"):
-			player.add_weapon_to_inventory(weapon_id, ammo_included)
+		# Immediately pick up the weapon when player enters the area
+		if body.has_method("add_weapon_to_inventory"):
+			body.add_weapon_to_inventory(weapon_id, ammo_included)
 			queue_free()
